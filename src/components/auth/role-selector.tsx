13c097d6d991a076168215ef41577d7cc6709772
Surender K M <aanadsurender21@gmail.com>
2025-09-25 18:20:31 +0000

    login or register button is not redirecting to login / registering pages

--- src/components/auth/role-selector.tsx
+++ src/components/auth/role-selector.tsx
@@ -1,34 +1,16 @@
 'use client';
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export function RoleSelector() {
-  const router = useRouter();
-
   return (
     <div className="grid md:grid-cols-2 gap-8 w-full max-w-sm">
-      <Button
-        size="lg"
-        className="w-full font-bold"
-        onClick={() => router.push('/login')}
-      >
-        Login
+      <Button size="lg" className="w-full font-bold" asChild>
+        <Link href="/login">Login</Link>
       </Button>
-      <Button
-        size="lg"
-        variant="secondary"
-        className="w-full font-bold"
-        onClick={() => router.push('/register')}
-      >
-        Register
+      <Button size="lg" variant="secondary" className="w-full font-bold" asChild>
+        <Link href="/register">Register</Link>
       </Button>
     </div>
   );
