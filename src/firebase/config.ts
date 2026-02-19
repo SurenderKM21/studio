306cd42de039d23aef0082656be96d71448a2b9e
Surender K M <aanadsurender21@gmail.com>
2026-02-19 11:09:50 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-project-id.firebaseapp.com",
-  projectId: "placeholder-project-id",
-  storageBucket: "placeholder-project-id.appspot.com",
-  messagingSenderId: "placeholder-sender-id",
-  appId: "placeholder-app-id"
+  "projectId": "studio-2288905817-e7b33",
+  "appId": "1:721792731959:web:ac4b5a8c2e0c77371a68b2",
+  "apiKey": "AIzaSyB95IDAoXcmb-K3FhgIxCc1IJyJFe9dXGc",
+  "authDomain": "studio-2288905817-e7b33.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "721792731959"
 };
