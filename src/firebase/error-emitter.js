27d255699993afe7203fadbc9a6103c0dd4dc29e
Surender K M <aanadsurender21@gmail.com>
2026-02-21 12:51:02 +0000

    are u using material ui?

---
+++ src/firebase/error-emitter.js
@@ -0,0 +1,34 @@
+
+'use client';
+
+/**
+ * A strongly-typed pub/sub event emitter for JavaScript.
+ */
+function createEventEmitter() {
+  const events = {};
+
+  return {
+    on(eventName, callback) {
+      if (!events[eventName]) {
+        events[eventName] = [];
+      }
+      events[eventName].push(callback);
+    },
+
+    off(eventName, callback) {
+      if (!events[eventName]) {
+        return;
+      }
+      events[eventName] = events[eventName].filter(cb => cb !== callback);
+    },
+
+    emit(eventName, data) {
+      if (!events[eventName]) {
+        return;
+      }
+      events[eventName].forEach(callback => callback(data));
+    },
+  };
+}
+
+export const errorEmitter = createEventEmitter();
