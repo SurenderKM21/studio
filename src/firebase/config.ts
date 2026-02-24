25293bea73abf28c721d6a5e5c8e787d5d3b90e1
Surender K M <aanadsurender21@gmail.com>
2026-02-24 17:16:14 +0000

    deletion

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-2288905817-e7b33",
-  "appId": "1:721792731959:web:ac4b5a8c2e0c77371a68b2",
-  "apiKey": "AIzaSyB95IDAoXcmb-K3FhgIxCc1IJyJFe9dXGc",
-  "authDomain": "studio-2288905817-e7b33.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "721792731959"
-};
--- src/lib/data.jsx
+++
@@ -1,8 +0,0 @@
-/**
- * @fileOverview Data service layer for EvacAI.
- */
-
-export const db = {
-  getZones: () => [],
-  getUsers: () => []
-};
